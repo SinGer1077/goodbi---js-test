53b22ad7070c12c1d11f90d883484edf2f1094f7
Vladimir Krasnoselskikh <kv@good.bi>
2020-08-27 17:35:10 +0500

    Init good.bi test frontend

--- src/App.js
+++ src/App.js
@@ -1,26 +1,100 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {makeStyles} from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
+import TextField from '@material-ui/core/TextField'
+import Button from "@material-ui/core/Button";
+import Card from "@material-ui/core/Card";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import CardHeader from "@material-ui/core/CardHeader"
+
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        display: 'flex',
+    },
+    appBackground: {
+        backgroundColor: '#f5f5f5',
+        minHeight: '100vh'
+    },
+    appBar: {
+        zIndex: theme.zIndex.drawer + 1,
+    },
+    drawerHeader: {
+        display: 'flex',
+        alignItems: 'center',
+        padding: theme.spacing(1, 2),
+        ...theme.mixins.toolbar,
+        justifyContent: 'flex-end',
+    },
+    content: {
+        flexGrow: 1,
+        padding: theme.spacing(3),
+    },
+
+    cardPage: {
+        maxWidth: 500
+    },
+
+    formPage: {
+        margin: theme.spacing(1),
+    },
+
+    formInput:{
+        width: '100%'
+    }
+}));
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const classes = useStyles();
+    return (
+        <div className={classes.appBackground}>
+            <AppBar position="fixed" className={classes.appBar}>
+                <Toolbar>
+                    <Typography variant="h6" noWrap>
+                        Узнай пол по имени
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+            <main>
+                <div className={classes.drawerHeader}/>
+                <Container>
+                    <Card className={classes.cardPage}>
+                        <form className={classes.formPage} noValidate autoComplete="off">
+                            <CardContent>
+                                <TextField className={classes.formInput} id="filled-basic" label="Введи свое транслитерированое имя (Латиницей)" variant="filled"/>
+                            </CardContent>
+                            <CardActions>
+                                <Button variant="contained" color="primary">
+                                    Узнать пол
+                                </Button>
+                            </CardActions>
+                        </form>
+                    </Card>
+                </Container>
+                <div className={classes.drawerHeader}/>
+                <Container className={classes.root}>
+
+                    <Card className={classes.cardPage}>
+                        <CardContent>
+                            <CardHeader title="Мужчина"/>
+                        </CardContent>
+                    </Card>
+
+                    <Card className={classes.cardPage}>
+                        <CardContent>
+                            <CardHeader title="Женщина"/>
+                        </CardContent>
+                    </Card>
+                </Container>
+
+            </main>
+        </div>
+    );
 }
 
 export default App;
