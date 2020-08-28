7f1707cf8163fd2df5f2b5da98281ecfcddee152
Singer <SinGer1077@github.com>
2020-08-28 22:56:30 +0500

    Сделанное задание

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect,useState} from 'react';
 import {makeStyles} from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
@@ -51,6 +51,39 @@ const useStyles = makeStyles((theme) => ({
 
 function App() {
     const classes = useStyles();
+    const [name,setName] = useState('');
+    const [fullName,setFullName] = useState('');
+    const [sex, setSex] = useState('');
+    const [genderText,setText] = useState('Неопределён');
+    const [card, setCard] = useState(true);
+    const updateName = e => {
+        setName(e.target.value);
+    }
+    const getName = e =>{
+        e.preventDefault();
+        setFullName(name);
+        setCard(false);
+    }
+    useEffect(()=>{
+        getInput();
+        }, [fullName, sex]);
+    const getGender = gender =>{
+        setSex(gender);
+    }
+    const getInput = async () => {
+        const response = await fetch(
+            'https://api.genderize.io?name='+fullName
+        );
+        const data = await response.json();
+        getGender(data.gender);
+        if (sex=='male')
+            setText('Мужчина')
+        if (sex=='female')
+            setText('Женщина')
+        if (sex==null)
+            setText('Неопределён')
+    };
+
     return (
         <div className={classes.appBackground}>
             <AppBar position="fixed" className={classes.appBar}>
@@ -66,10 +99,12 @@ function App() {
                     <Card className={classes.cardPage}>
                         <form className={classes.formPage} noValidate autoComplete="off">
                             <CardContent>
-                                <TextField className={classes.formInput} id="filled-basic" label="Введи свое транслитерированое имя (Латиницей)" variant="filled"/>
+                                <TextField className={classes.formInput} id="filled-basic"
+                                           label="Введи свое транслитерированое имя (Латиницей)" variant="filled"
+                                onChange = {updateName}/>
                             </CardContent>
                             <CardActions>
-                                <Button variant="contained" color="primary">
+                                <Button onClick={getName} variant="contained" color="primary">
                                     Узнать пол
                                 </Button>
                             </CardActions>
@@ -77,17 +112,10 @@ function App() {
                     </Card>
                 </Container>
                 <div className={classes.drawerHeader}/>
-                <Container className={classes.root}>
-
-                    <Card className={classes.cardPage}>
+                <Container className={classes.root} >
+                    <Card className={classes.cardPage} hidden={card}>
                         <CardContent>
-                            <CardHeader title="Мужчина"/>
-                        </CardContent>
-                    </Card>
-
-                    <Card className={classes.cardPage}>
-                        <CardContent>
-                            <CardHeader title="Женщина"/>
+                            <CardHeader title={genderText}/>
                         </CardContent>
                     </Card>
                 </Container>
